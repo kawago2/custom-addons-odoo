6117bb54df64476570f8f652476d9d6f9a64a5c5
kawago <itkawago@gmail.com>
2022-08-30 10:32:36 +0700

    add wikumart & add backup-wiku mart, edit wikumart

--- custom/addons/barang/models/barang.py
+++ custom/addons/barang/models/barang.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+
 from odoo import models, fields
 
 
@@ -7,7 +8,6 @@ class BarangBarang(models.Model):
     _name = "barang.barang"
     _description = "Testing add module"
 
-    name = fields.Char()
-    value = fields.Integer()
-    value2 = fields.Float(compute="_value_pc", store=True)
+    name = fields.Char(string='Nama Barang', required=True)
+    harga = fields.Float(string='Harga', required=True)
     description = fields.Text()
